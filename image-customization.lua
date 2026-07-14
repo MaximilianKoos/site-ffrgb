015c8b18cff00256801ecd4022410b1da19dfe21
Maximilian Koos <58424706+MaximilianKoos@users.noreply.github.com>
2026-07-14 15:57:02 +0200

    Update image-customization.lua

--- image-customization.lua
+++ image-customization.lua
@@ -1,31 +1,29 @@
 -- Gluon image customization for Freifunk Regensburg fork
--- Migration of former GLUON_FEATURES for Gluon >= 2023.2
+-- for Gluon >= 2023.2
 
 local M = {}
 
 function M.register(common)
-  -- Basis: Autoupdater aktiv
   common.features.autoupdater = true
-
-  -- Config-Mode Features
   common.features["config-mode-domain-select"] = true
   common.features["config-mode-geo-location-osm"] = true
   common.features["config-mode-mesh-vpn"] = true
-
-  -- Ebtables-Filter
   common.features["ebtables-filter-multicast"] = true
   common.features["ebtables-filter-ra-dhcp"] = true
   common.features["ebtables-source-filter"] = true
-
-  -- Mesh / Batman-adv 15
   common.features["mesh-batman-adv-15"] = true
-
-  -- Status/Respondd/Web
   common.features["respondd"] = true
   common.features["status-page"] = true
   common.features["web-advanced"] = true
   common.features["web-private-wifi"] = true
   common.features["web-wizard"] = true
+
+  common.packages["gluon-segment-mover"] = true
+  common.packages["iwinfo"] = true
+  common.packages["gluon-ssid-changer"] = true
+  common.packages["respondd-module-airtime"] = true
+  common.packages["gluon-mesh-vpn-wireguard-vxlan"] = true
+  common.packages["ffrgb-banner"] = true
 end
 
 return M
