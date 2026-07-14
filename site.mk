b81b75ecbad4c429ca6858254f6a1509d7b962dc
Maximilian Koos <58424706+MaximilianKoos@users.noreply.github.com>
2026-07-14 16:02:06 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -2,13 +2,10 @@ GLUON_MULTIDOMAIN=1
 GLUON_DEPRECATED ?= upgrade
 
 DEFAULT_GLUON_RELEASE := snapshot~$(shell date '+%Y%m%d')
-# Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 GLUON_PRIORITY ?= 0
 GLUON_REGION ?= eu
 GLUON_LANGS ?= en de
-
-# additional packages
 
 INCLUDE_USB := \
     usbutils
@@ -128,39 +125,6 @@ EXCLUDE_TLS := \
     -ca-bundle \
     -libustream-openssl
 
-ifeq ($(GLUON_TARGET),ath79-generic)
-    GLUON_SITE_PACKAGES += $(INCLUDE_TLS)
-    GLUON_devolo-wifi-pro-1750e_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_gl.inet-gl-ar150_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_gl.inet-gl-ar300m-lite_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_gl.inet-gl-ar750_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_joy-it-jt-or750i_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_netgear-wndr3700-v2_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_tp-link-archer-a7-v5_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_tp-link-archer-c5-v1_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_tp-link-archer-c7-v2_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_tp-link-archer-c7-v5_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_tp-link-archer-c59-v1_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_tp-link-tl-wr842n-v3_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_tp-link-tl-wr1043nd-v4_SITE_PACKAGES += $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-    GLUON_d-link-dir825b1_SITE_PACKAGES += $(EXCLUDE_TLS)
-    GLUON_tp-link-re355-v1_SITE_PACKAGES += $(EXCLUDE_TLS)
-    GLUON_tp-link-re450-v1_SITE_PACKAGES += $(EXCLUDE_USB) $(EXCLUDE_USB_NET) $(EXCLUDE_USB_SERIAL) $(EXCLUDE_USB_STORAGE)
-endif
-
-# no pkglists for target ath79-mikrotik
-
-
-ifeq ($(GLUON_TARGET),ath79-nand)
-    GLUON_SITE_PACKAGES += $(INCLUDE_TLS) $(INCLUDE_USB) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-endif
-
-ifeq ($(GLUON_TARGET),bcm27xx-bcm2708)
-    GLUON_SITE_PACKAGES += $(INCLUDE_TLS) $(INCLUDE_USB) $(INCLUDE_USB_HID) $(INCLUDE_USB_NET) $(INCLUDE_USB_SERIAL) $(INCLUDE_USB_STORAGE)
-endif
-
-# some extra sauce to make offloader/kvm images extra spicy
-
 USB_PACKAGES_BASIC := \
 	kmod-usb-core \
 	kmod-usb2
@@ -187,7 +151,6 @@ USB_PACKAGES_STORAGE := \
 	kmod-nls-utf8 \
 	swap-utils
 
-# network support for USB devices
 USB_PACKAGES_NET := \
 	kmod-mii \
 	kmod-nls-base \
@@ -216,7 +179,7 @@ USB_PACKAGES_NET := \
 USB_PACKAGES_NET_ADD := \
 	kmod-rtl8192cu \
 	kmod-rtl8187 \
-	kmod-ath9k-htc  \
+	kmod-ath9k-htc \
 	kmod-ath9k-common \
 	kmod-ath \
 	kmod-rt73-usb \
@@ -245,35 +208,3 @@ TOOLS_PACKAGES := \
 	tcpdump \
 	usbutils \
 	vnstat
-
-# x86-generic
-ifeq ($(GLUON_TARGET),x86-generic)
-# support the usb stack on x86 devices
-# and add a few common USB and PCI NICs
-GLUON_SITE_PACKAGES += \
-	kmod-usb-hid \
-	kmod-hid-generic \
-	kmod-button-hotplug \
-	$(USB_PACKAGES_BASIC) \
-	$(USB_PACKAGES_STORAGE) \
-	$(USB_PACKAGES_NET) \
-	$(PCI_PACKAGES_NET) \
-	$(TOOLS_PACKAGES) \
-	$(USB_PACKAGES_NET_ADD)
-endif
-
-# x86-64
-ifeq ($(GLUON_TARGET),x86-64)
-# support the usb stack on x86-64 devices
-# and add a few common USB and PCI NICs
-GLUON_SITE_PACKAGES += \
-	kmod-usb-hid \
-	kmod-hid-generic \
-	kmod-button-hotplug \
-	$(USB_PACKAGES_BASIC) \
-	$(USB_PACKAGES_STORAGE) \
-	$(USB_PACKAGES_NET) \
-	$(PCI_PACKAGES_NET) \
-	$(TOOLS_PACKAGES) \
-	$(USB_PACKAGES_NET_ADD)
-endif
